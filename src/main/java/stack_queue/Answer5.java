bd3d521c6180fa99a259d13cf358badd508ee500
Ppojin <25076967+Ppojin@users.noreply.github.com>
2020-03-18 20:38:34 +0900

    [mod] 출처표시

--- src/main/java/stack_queue/Answer5.java
+++ src/main/java/stack_queue/Answer5.java
@@ -1,23 +1,22 @@
 package stack_queue;
 
-import java.util.ArrayList;
-
 public class Answer5 {
     public int solution(String arrangement) {
-        ArrayList<Integer> resultArray = new ArrayList<>();
         int result = 0;
         int len = arrangement.length();
+        int sCount = 0;
         for(int i = 0 ; i < len ; i++){
-            String val = (i == len)? arrangement.substring(i) : arrangement.substring(i,i+1);
-            String laser = (i+1 == len)? arrangement.substring(i) : arrangement.substring(i,i+2);
-            if (laser.equals("()")){
-                resultArray.replaceAll(n -> n+1);
+            boolean laser = arrangement.charAt(i) == '(' && arrangement.charAt(i + 1) == ')';
+            if(laser){
+                result += sCount;
                 i++;
             } else {
-                if(val.equals("(")){
-                    resultArray.add(1);
-                } else if (val.equals(")")){
-                    result += resultArray.remove(resultArray.size()-1);
+                char val = arrangement.charAt(i);
+                if(val == '('){
+                    sCount += 1;
+                    result += 1;
+                } else if (val == ')'){
+                    sCount -= 1;
                 }
             }
         }
